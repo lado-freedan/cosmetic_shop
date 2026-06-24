b91eba35676c507614089d8b2e34fd45c6218eb1
Yourname <you@email.com>
2026-06-24 17:43:02 +0400

    feat: add welcome message to root url

--- config/urls.py
+++ config/urls.py
@@ -22,8 +22,23 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from django.http import HttpResponse
+
+
+def home_page(reuest):
+    html_content = """
+    <div style="font-family: sans-serif; text-align: center; margin-top: 100px">
+        <h1 style="color: #4A5568;">Cosmetic ShopAPI</h1>
+        <p style="color: #718096; font-size: 18px;">The Backend Server is Up and running successfully!</p>
+        <div style="margin-top: 20px;">
+            <a href="/api/products/" style="color: #3182CE; text-decoration: none; font-weight: bold;">View Products API</a>
+        </div>
+    </div>
+    """
+    return HttpResponse(html_content)
 
 urlpatterns = [
+    path("", home_page),
     path('admin/', admin.site.urls),
     path('api/products/', include('products.urls')),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
